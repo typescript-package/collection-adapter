064698722c7be9756d180711bda981d76d2e9f0c
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-01-02 06:05:32 +0000

    feat(`ArrayAdapter`): add adapter of `array` type.

---
+++ src/lib/array.adapter.ts
@@ -0,0 +1,71 @@
+// Interface.
+import { CollectionAdapter } from '@typedly/collection';
+/**
+ * @description The `Array` collection adapter.
+ * @export
+ * @class ArrayAdapter
+ * @template E The type of the elements in the Array.
+ * @template [T=Array<E>] The type of the underlying Array collection.
+ * @implements {CollectionAdapter<E, T>}
+ */
+export class ArrayAdapter<E> implements CollectionAdapter<E, Array<E>> {
+  public version: string = '1.0.0';
+  get [Symbol.toStringTag](): string {
+    return 'ArrayAdapter';
+  }
+  [Symbol.iterator](): IterableIterator<E> {
+    return this.#collection[Symbol.iterator]();
+  }
+  protected get collection(): Array<E> {
+    return this.#collection;
+  }
+  public get size(): number {
+    return this.collection.length;
+  }
+  public get value(): Array<E> {
+    return this.collection;
+  }
+  #collection: Array<E>;
+  constructor(...collection: E[]) {
+    this.#collection = new Array(...collection);
+  }
+  public add(...element: E[]): this {
+    return element.forEach(e => this.collection.push(e)), this;
+  }
+  public clear(): this {
+    return this.collection.length = 0, this;
+  }
+  public destroy(): this {
+    return this.clear(), (this.#collection = null as any), this;
+  }
+  public delete(...element: E[]): boolean {
+    return element.every(e => {
+      const index = this.collection.indexOf(e);
+      if (index === -1) return false;
+      this.collection.splice(index, 1);
+      return true;
+    });
+  }
+  public forEach(
+    callbackfn: (element: E, nextElement: E, collection: CollectionAdapter<E, Array<E>>) => void,
+    thisArg?: any
+  ): this {
+    return this.collection.forEach(
+      (element, nextElement, array) => callbackfn.call(
+        thisArg,
+        element,
+        array.length > nextElement + 1 ? array[nextElement + 1] : undefined as E,
+        this as any
+      )),
+      this;
+  }
+  public has(...element: E[]): boolean {
+    return element.every(e => this.collection.indexOf(e) !== -1);
+  }
+  public lock(): this {
+    return Object.freeze(this.collection), this;
+  }
+  public set(value: Array<E>): this {
+    return (this.#collection = value), this;
+  }
+}
