44e2fe75df69d88cc5f77ec2d13f798e11117336
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-02-25 17:29:51 +0000

    test(`ArrayAdapter`, `SetAdapter`): simplify collection initialization by removing unnecessary options

--- src/test/set.adapter.spec.ts
+++ src/test/set.adapter.spec.ts
@@ -1,7 +1,7 @@
 import { Collection } from '@typescript-package/collection';
 import { SetAdapter } from '../lib';
 
-const collection = new Collection({async: false, value: new Set([1, 2, 3])}, SetAdapter, 1, 2, 3);
+const collection = new Collection(SetAdapter, 1, 2, 3);
 
 // Adds.
 collection.add(27, 29, 31, 33);
